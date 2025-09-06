66f78817c423968e14757bf6c5fc4c07594f1053
rahulverma9449 <rahulverma9449@gmail.com>
2025-09-06 15:59:03 +0530

    Apply .gitignore and remove unwanted files

---
+++ sso_project/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import LoginButton from "./components/LoginButton";
+ 
+
+function App() {
+  return (
+    <div>
+      <Header />
+     
+      <LoginButton />
+    
+    </div>
+  );
+}
+
+export default App;
